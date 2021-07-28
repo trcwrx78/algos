c763e091db8a2459c50fc5917865bea9d4d8cae9
Torrence Cole <trcole@gmail.com>
2021-07-28 10:15:13 -0400

    add three methods, getFirst, getLast, and size

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -17,6 +17,37 @@ class LinkedList {
   insertFirst(data) {
     this.head = new Node(data, this.head);
   }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
